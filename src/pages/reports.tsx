f48284cca56ff3bc8e4d061ec4d03a436c54226f
Adory Vo <business@adoryvo.com>
2022-11-10 02:23:22 -0800

    Add database connection

---
+++ src/pages/reports.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next'
+
+import prisma from '../lib/prisma'
+import { makeSerializable } from '../lib/util'
+
+
+interface Props {
+  feed: Report[]
+}
+
+interface Report {
+  id: string
+}
+
+export default function Reports(props: Props) {
+  console.log(props.feed)
+
+  return (
+    <>
+
+      {props.feed.map((report) => (
+        <div key={report.id}>
+          {report.id}
+        </div>
+      ))}
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.report.findMany()
+  return {
+    props: { feed: makeSerializable(feed) },
+    revalidate: 10,
+  }
+}
