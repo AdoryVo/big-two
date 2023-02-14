a5fc93bf2df6c84b5be6ca45872f4f200c8cf665
Adory Vo <business@adoryvo.com>
2023-02-13 16:19:29 -0800

    Removed author field

---
+++ prisma/migrations/20230214001424_removed_author/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `author` on the `Report` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Report" DROP COLUMN "author";
