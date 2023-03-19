5a27d02eaf77d84f7dd3ea54e625fa7d365f5a53
Michael Huo <huom7473@g.ucla.edu>
2023-03-18 17:29:25 -0700

    fix changing card theme requiring refresh

--- src/utils/hooks/useTheme.ts
+++ src/utils/hooks/useTheme.ts
@@ -1,20 +1,28 @@
-import { useEffect, useState } from 'react'
 
 import type { Theme } from '@utils/theme'
 import {
   DEFAULT_THEME, getThemeOption, setThemeOption,
 } from '@utils/theme'
+import _ from 'lodash'
+import { useEffect, useState } from 'react'
 
 export function useTheme(): [Theme, (update: Theme) => void] {
   const [theme, setTheme] = useState<Theme>(DEFAULT_THEME)
 
   useEffect(() => {
     // Set theme from local storage
-    setTheme({
+    const localStorageTheme = {
       cardTheme: getThemeOption('cardTheme'),
       colorScheme: getThemeOption('colorScheme'),
-    })
-  }, [])
+    }
+
+    if (!_.isEqual(theme, localStorageTheme)) {
+      setTheme({
+        cardTheme: getThemeOption('cardTheme'),
+        colorScheme: getThemeOption('colorScheme'),
+      })
+    }
+  })
 
   function updateTheme(update: Theme) {
     setTheme(update)
