a653a32baee24fb3a050cdfc507d0e7cb79fbe42
Adory Vo <business@adoryvo.com>
2023-10-22 11:39:56 -0700

    Fix ESLint error

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -3,7 +3,7 @@ import { Link, Text, type TextProps, Tooltip } from '@chakra-ui/react'
 export default function Version(props: TextProps) {
   return (
     <Tooltip hasArrow label="See changelog" placement="right" ms={2}>
-      <Text position={["absolute", "fixed"]} bottom={0} left={0} m={[0, 4]} w={['100%', 'auto']} textAlign={['center', 'left']} {...props}>
+      <Text position={['absolute', 'fixed']} bottom={0} left={0} m={[0, 4]} w={['100%', 'auto']} textAlign={['center', 'left']} {...props}>
         <Link href="https://github.com/AdoryVo/big-two/blob/main/CHANGELOG.md" isExternal>
           v1.0.1
         </Link>
