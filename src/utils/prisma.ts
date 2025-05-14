df63d196e5bb756f91b852f0e2d9ace29313aae8
Adory Vo <business@adoryvo.com>
2025-05-13 22:28:10 -0700

    Update to v1.2.0; Code cleanup

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Type Variations
-const gameWithPlayers = Prisma.validator<Prisma.GameArgs>()({
+const gameWithPlayers = Prisma.validator<Prisma.GameDefaultArgs>()({
   include: { players: true, currentPlayer: true, settings: true },
 });
 export type GameWithPlayers = Prisma.GameGetPayload<typeof gameWithPlayers>;
