7a536a6147c250f54bfc30bd0166a0fdff4cd37e
Adory Vo <business@adoryvo.com>
2025-05-31 16:15:59 -0700

    fix: prevent duplicate player id keys by changing to redacted ids

--- src/pages/api/[gameId]/game.ts
+++ src/pages/api/[gameId]/game.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { generateSlug } from 'random-word-slugs';
 
 import prisma from '@utils/prisma';
 
@@ -30,29 +31,31 @@ export default async function handler(
 
   const playerId = req.cookies[game.id];
   const player = game.players.find((player) => player.id === playerId);
-  if (playerId && player && player.finishedRank === 0) {
-    // If a remaining player is requesting game data, obscure other players' id's & cards
-    for (const player of game.players) {
-      if (player.id !== playerId) {
-        player.id = '';
-        player.hand = player.hand.map(() => '');
-      }
-    }
-  } else {
-    // Obscure ID's from spectating players
-    for (const player of game.players) {
-      // Do not obscure id if this player is the spectator
-      if (playerId && player.id === playerId) {
-        continue
-      }
-
-      player.id = '';
-    }
-
-    if (!game.settings.spectating) {
-      // Hide all cards if spectating is not allowed
+  if (game.currentPlayer !== null) {
+    if (playerId && player && player.finishedRank === 0) {
+      // If a remaining player is requesting game data, obscure other players' id's & cards
       for (const player of game.players) {
-        player.hand = player.hand.map(() => '');
+        if (player.id !== playerId) {
+          player.id = `redacted-${generateSlug()}`;
+          player.hand = player.hand.map(() => '');
+        }
+      }
+    } else {
+      // Obscure ID's from spectating players
+      for (const player of game.players) {
+        // Do not obscure id if this player is the spectator
+        if (playerId && player.id === playerId) {
+          continue;
+        }
+
+        player.id = `redacted-${generateSlug()}`;
+      }
+
+      if (!game.settings.spectating) {
+        // Hide all cards if spectating is not allowed
+        for (const player of game.players) {
+          player.hand = player.hand.map(() => '');
+        }
       }
     }
   }
